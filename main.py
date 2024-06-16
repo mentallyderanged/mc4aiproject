b5fe4f46034fb3755c868517cb4b245362fc1d1d
Daniel <131584933+mentallyderanged@users.noreply.github.com>
2024-06-17 06:55:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ from tensorflow.keras.layers import Dense, Activation, Input, Flatten
 from tensorflow.keras.utils import set_random_seed
 from tensorflow.keras.backend import clear_session
 
-# Set random seed for reproducibility
-set_random_seed(42)
-
-# Clear any previous models
 clear_session()
+set_random_seed(42)
+np.random.seed(42)
 
 ds_path = input("Enter the path to the sample dataset directory: ").strip()
 testsize = float(input("Enter the test size (between 0 and 1): ").strip())
+epoch = int(input("Enter the number of epochs: ").strip())
 
 folders = os.listdir(ds_path)
 label_mapping = {folder: i for i, folder in enumerate(folders)}
 X, y = [], []
 
-# read files in folder
+# Read files in folder
 for folder in folders:
     files = os.listdir(os.path.join(ds_path, folder))
     for f in files:
@@ -33,13 +32,13 @@ for folder in folders:
             X.append(img)
             y.append(label_mapping[folder])
 
-# convert lists to numpy arrays
+# Convert lists to numpy arrays
 X = np.array(X)
 y = np.array(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testsize, shuffle=True)
 
-# normalize the data
+# Normalize the data
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
@@ -61,3 +60,28 @@ y_train_ohe = to_categorical(y_train, num_classes=y.max() + 1)
 y_test_ohe = to_categorical(y_test, num_classes=y.max() + 1)
 
 print(y_train.shape, y_train_ohe.shape, y_test.shape, y_test_ohe.shape)
+
+# Define the model
+model = Sequential()
+model.add(Input(shape=X_train.shape[1:]))
+model.add(Flatten())
+model.add(Dense(y.max()+1, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train_ohe, epochs = epoch, verbose=1)
+
+# test
+# plt.figure(figsize=(8,4))
+# 
+# plt.subplot(1,2,1)
+# plt.title('Loss')
+# plt.xlabel('Epochs')
+# plt.plot(history.history['loss'])
+# 
+# plt.subplot(1,2,2)
+# plt.title('Accuracy')
+# plt.xlabel('Epochs')
+# plt.plot(history.history['accuracy'])
+# 
+# plt.show()
