2d1b23a67c9b6ef828c2a35ba09b97fec309eb15
Daniel <131584933+mentallyderanged@users.noreply.github.com>
2024-06-16 11:11:27 +0700

    Update randomsampleselection.py

--- randomsampleselection.py
+++ randomsampleselection.py
@@ -6,7 +6,7 @@ import random
 # Set the source directory containing the images
 source_dir = input("Enter the path to the source directory: ")
 new_dataset_dir = input("Enter the desired path to the sample dataset directory: ")
-
+number_of_images = int(input("Enter the number of images to be sampled: "))
 os.makedirs(new_dataset_dir, exist_ok=True)
 
 for folders in os.listdir(source_dir):
@@ -19,11 +19,15 @@ for folders in os.listdir(source_dir):
         # Get a list of all image files in the source folder
         image_files = [f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
         
-        # Randomly select 1000 images
-        selected_images = random.sample(image_files, 1000) if len(image_files) >= 1000 else image_files
+        # Randomly select number of images wanted from the source folder
+        selected_images = random.sample(image_files, number_of_images) if len(image_files) >= number_of_images else image_files
         
         # Copy the selected images to the new dataset folder
         for image in selected_images:
             source_image_path = os.path.join(folder_path, image)
             destination_image_path = os.path.join(new_folder_path, image)
-            shutil.copy(source_image_path, destination_image_path)
+            try:
+                shutil.copy(source_image_path, destination_image_path)
+                print(f"Copied {image} to {new_folder_path}")
+            except IOError as e:
+                print(f"Could not copy {image}: {e}")
